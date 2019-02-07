c81c66681b2a5df8392bcaed70661de7fd9e0dcd
Andrew Piper <andrew@threax.com>
2019-02-07 18:18:22 -0500

    Started adding table to index. Moved CodeMirrorEditor to its own file.

---
+++ Notes/Client/Libs/CodeMirrorEditor.ts
@@ -0,0 +1,60 @@
+import * as controller from "hr.controller";
+import * as client from 'clientlibs.ServiceClient';
+import { TimedTrigger } from 'hr.timedtrigger';
+
+declare function CodeMirror(element, config);
+
+export function addServices(services: controller.ServiceCollection) {
+    services.addTransient(CodeMirrorEditor, CodeMirrorEditor);
+}
+
+export function createStandard(builder: controller.InjectedControllerBuilder) {
+    builder.create("codemirror", CodeMirrorEditor);
+}
+
+export class CodeMirrorEditor {
+    public static get InjectorArgs(): controller.DiFunction<any>[] {
+        return [controller.BindingCollection, client.EntryPointInjector];
+    }
+
+    private codeMirror: any;
+    private handle: HTMLElement;
+    private trigger: TimedTrigger<any>;
+    private note: client.NoteResult;
+
+    public constructor(bindings: controller.BindingCollection, private injector: client.EntryPointInjector) {
+        this.handle = bindings.getHandle("codemirror");
+        this.trigger = new TimedTrigger<any>(500);
+
+        this.setup();
+    }
+
+    private async setup(): Promise<void> {
+        var entry = await this.injector.load();
+        var notes = await entry.listNotes({});
+        var noteListing = notes.items[0];
+        this.note = await noteListing.refresh();
+
+        this.codeMirror = CodeMirror(this.handle, {
+            mode: "markdown",
+            theme: "threax-notes",
+            lineNumbers: true,
+            lineWrapping: true,
+            value: this.note.data.text
+        });
+
+        var self = this;
+        this.codeMirror.on("changes", (instance, changes) => {
+            self.trigger.fire(self);
+        });
+
+        //Do this here so everything else is registered
+        this.trigger.addListener(a => this.save());
+    }
+
+    private async save(): Promise<void> {
+        this.note = await this.note.update({
+            text: this.codeMirror.getValue()
+        });
+    }
+}
--- Notes/Views/Home/Index.ts
+++ Notes/Views/Home/Index.ts
@@ -1,57 +1,15 @@
-import * as controller from "hr.controller";
-import * as client from 'clientlibs.ServiceClient';
+import * as standardCrudPage from 'hr.widgets.StandardCrudPage';
 import * as startup from 'clientlibs.startup';
-import { TimedTrigger } from 'hr.timedtrigger';
-
-declare function CodeMirror(element, config);
-
-class CodeMirrorEditor {
-    public static get InjectorArgs(): controller.DiFunction<any>[] {
-        return [controller.BindingCollection, client.EntryPointInjector];
-    }
-
-    private codeMirror: any;
-    private handle: HTMLElement;
-    private trigger: TimedTrigger<any>;
-    private note: client.NoteResult;
-
-    public constructor(bindings: controller.BindingCollection, private injector: client.EntryPointInjector) {
-        this.handle = bindings.getHandle("codemirror");
-        this.trigger = new TimedTrigger<any>(500);
-
-        this.setup();
-    }
-
-    private async setup(): Promise<void> {
-        var entry = await this.injector.load();
-        var notes = await entry.listNotes({});
-        var noteListing = notes.items[0];
-        this.note = await noteListing.refresh();
-
-        this.codeMirror = CodeMirror(this.handle, {
-            mode: "markdown",
-            theme: "threax-notes",
-            lineNumbers: true,
-            lineWrapping: true,
-            value: this.note.data.text
-        });
-
-        var self = this;
-        this.codeMirror.on("changes", (instance, changes) => {
-            self.trigger.fire(self);
-        });
-
-        //Do this here so everything else is registered
-        this.trigger.addListener(a => this.save());
-    }
-
-    private async save(): Promise<void> {
-        this.note = await this.note.update({
-            text: this.codeMirror.getValue()
-        });
-    }
-}
+//import * as deepLink from 'hr.deeplink';
+import { NoteCrudInjector } from 'clientlibs.NoteCrudInjector';
+import * as codemirroreditor from 'clientlibs.CodeMirrorEditor';
 
 var builder = startup.createBuilder();
-builder.Services.addTransient(CodeMirrorEditor, CodeMirrorEditor);
-builder.create("codemirror", CodeMirrorEditor);
+codemirroreditor.addServices(builder.Services);
+codemirroreditor.createStandard(builder);
+
+var injector = NoteCrudInjector;
+
+//deepLink.addServices(builder.Services);
+standardCrudPage.addServices(builder, injector);
+standardCrudPage.createControllers(builder, new standardCrudPage.Settings());
