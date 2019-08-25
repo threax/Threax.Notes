86c6b26dae07b3c2e014a03ab95654c2f251d38b
Andrew Piper <andrew@threax.com>
2019-08-25 06:20:35 -0400

    Added app start.

--- Notes/Controllers/HomeController.cs
+++ Notes/Controllers/HomeController.cs
@@ -21,6 +21,12 @@ namespace Notes.Controllers
             return View();
         }
 
+        [AllowAnonymous]
+        public IActionResult AppStart()
+        {
+            return View();
+        }
+
         //The other view action methods are in the additional partial classes for HomeController, expand the node for
         //this class to see them.
     }
