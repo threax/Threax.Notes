fe52b5dde43a2bcbb854845152459df6493ed531
Andrew Piper <andrew@threax.com>
2020-06-08 16:15:30 -0400

    Added k8s deploy config.

--- Notes/Startup.cs
+++ Notes/Startup.cs
@@ -51,6 +51,7 @@ namespace Notes
             Configuration.Bind("AppConfig", appConfig);
             Configuration.Bind("ClientConfig", clientConfig);
             Configuration.Bind("Cors", corsOptions);
+            Configuration.Define("K8sDeploy", typeof(Threax.K8sDeploy.Config.K8sDeployConfig));
         }
 
         public SchemaConfigurationBinder Configuration { get; }
