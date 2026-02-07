21867f2ae2c12818f0a27dbe41980a808875d661
Andrew Piper <andrew@threax.com>
2026-02-07 16:27:10 -0500

    Call UseForwardedHeaders with no args.

--- Notes/Startup.cs
+++ Notes/Startup.cs
@@ -212,6 +212,8 @@ namespace Notes
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
+            app.UseForwardedHeaders();
+
             app.UseUrlFix(o =>
             {
                 o.CorrectPathBase = appConfig.PathBase;
